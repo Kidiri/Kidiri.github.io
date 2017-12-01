843d2279a871bba2a581f377bbc67c7a60b3b2f3
Michiel Verlinden <michielverlinden@hotmail.com>
2017-12-01 00:36:37 +0000

    Added removal script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-window.onload(Func());
-
-    let paragraph=document.getElementById("paragraph");
-function Func(){
-    paragraph.textContent="Welcome.";
+function RemoveUnwantedScript(){
+    var head=document.getElementsByTagName("script");
+    for (var i = head.length; i--; ) {
+        var script=head[i];
+        if (script.id==="") {
+            script.parentNode.removeChild(script);
+        }
+    }
+    var element = document.getElementById("pExecArea");
+    if (document.contains(element)) {
+        element.parentNode.removeChild(element);
+    }
 }
